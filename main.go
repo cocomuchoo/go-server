c695a033902d0e70411107d1defb916a3db5516f
cocomuchoo <figueroa.mgp@gmail.com>
2025-05-28 16:11:27 +0800

    Update

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name: %s\n", name)
